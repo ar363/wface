ab7ff62f301a7c2951780b7641e235c62997c4b2
Aditya Raj <aditya.rajb@gmail.com>
2024-12-13 00:11:15 +0530

    storage

--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,5 @@
   
   <NuxtLink class="m-4 rounded-md bg-lime-100 p-4 inline-block" href="/recognize">Recognise</NuxtLink>
   <NuxtLink class="m-4 rounded-md bg-lime-100 p-4 inline-block" href="/addface">Add face</NuxtLink>
+  <NuxtLink class="m-4 rounded-md bg-lime-100 p-4 inline-block" href="/storage">Storage</NuxtLink>
 </template>
---
+++ pages/storage/index.client.vue
@@ -0,0 +1,18 @@
+<script setup>
+const faces = ref(JSON.parse(localStorage.getItem('faces') || "[]"))
+
+const removeFace = (name) => {
+  const newFaces = faces.value.filter(face => face.name !== name)
+  localStorage.setItem('faces', JSON.stringify(newFaces))
+  faces.value = newFaces
+}
+</script>
+
+<template>
+  <div class="mx-auto p-4 max-w-screen-sm">
+    <h1 class="text-2xl font-bold">Storage</h1>
+    <ul class="list-disc">
+      <li v-for="face in faces" :key="face.name">{{ face.name }} [ <button class="text-red-500" @click="removeFace(face.name)">Remove</button> ]</li>
+    </ul>
+  </div>
+</template>
